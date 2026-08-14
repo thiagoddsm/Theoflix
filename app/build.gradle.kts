0825b602866ff0fe5f0bc08b344f93705182b1b0
thiagoddsm <thiagodossantosmatias@gmail.com>
2026-08-14 11:05:03 -0300

    ci: Adiciona workflow do GitHub Actions para compilar APK e publicar Release automaticamente

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.example"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
